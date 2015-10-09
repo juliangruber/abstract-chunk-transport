ebafef211f81f149854646bbae698f653de76899
Julian Gruber <julian@juliangruber.com>
2015-10-09 14:43:17 +0200

    example: tcp

--- example.js
+++ example.js
@@ -1,17 +1,24 @@
 var Store = require('memory-chunk-store');
 var transport = require('./');
+var net = require('net');
 
 var store = new Store(16);
 
-var server = transport.server(store);
-var client = transport.client();
+var server = net.createServer(function(con){
+  con.pipe(transport.server(store)).pipe(con);
+});
+server.listen(9000, function(){
+  var con = net.connect(9000);
 
-client.pipe(server).pipe(client);
+  var client = transport.client();
+  client.pipe(con).pipe(client);
 
-client.put(0, Buffer('0123456789abcdef'), function(err){
-  if (err) throw err;
-  client.get(0, function(err, buf){
+  client.put(0, Buffer('0123456789abcdef'), function(err){
     if (err) throw err;
-    console.log('BUF', buf, buf.toString());
+    client.get(0, function(err, buf){
+      if (err) throw err;
+      console.log('BUF', buf, buf.toString());
+    });
   });
 });
+
